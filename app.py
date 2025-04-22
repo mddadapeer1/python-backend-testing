60de2761d1b129ccdaa5a3629ee9c976ca8c63c1
Md Dadapeer <127332734+mddadapeer1@users.noreply.github.com>
2025-04-22 17:04:25 +0530

    Update app.py

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ app = Flask(__name__)
 
 # Database Configuration
 db_config = {
-    'host': 'database-1.cytea2me860o.us-east-1.rds.amazonaws.com',
+    'host': 'database-1.cytea2me860o.us-east-1.rds.amazonaws.com',   ## add here the data base endpoint from the AWS RDS ##
     'user': 'admin',
     'password': 'Cloud123',
     'database': 'dev'  # Change to your actual database name
